3603c786013bf6b0279e4b1983799a17975aac03
James Sparling <jamesdsparling@gmail.com>
2023-10-15 16:34:17 +0100

    fsharp formatting changes

--- MathsSolverBackend/interpreter.fs
+++ MathsSolverBackend/interpreter.fs
@@ -3,42 +3,43 @@
 
 namespace MathSolverBackend
 
-    module Interpreter = 
+module Interpreter =
 
-        open Lexer
-        open ShuntingYard
+    open Lexer
+    open ShuntingYard
 
-        // Evaluate result of expression
-        let evaluatePostfix tokens =
-            let mutable stack = []
-            for token in tokens do
-                match token with
-                | NUMBER n -> stack <- n :: stack
-                | PLUS ->
-                    let a = List.head stack
-                    stack <- List.tail stack
-                    let b = List.head stack
-                    stack <- List.tail stack
-                    stack <- (a + b) :: stack
-                | TIMES ->
-                    let a = List.head stack
-                    stack <- List.tail stack
-                    let b = List.head stack
-                    stack <- List.tail stack
-                    stack <- (a * b) :: stack
-                | _ -> ()
-            List.head stack
+    // Evaluate result of expression
+    let evaluatePostfix tokens =
+        let mutable stack = []
 
-        // Interpret input string
-        let interpret input =
-            let tokens = tokenize input
-            let postfix = infixToPostfix tokens
-            evaluatePostfix postfix
+        for token in tokens do
+            match token with
+            | NUMBER n -> stack <- n :: stack
+            | PLUS ->
+                let a = List.head stack
+                stack <- List.tail stack
+                let b = List.head stack
+                stack <- List.tail stack
+                stack <- (a + b) :: stack
+            | TIMES ->
+                let a = List.head stack
+                stack <- List.tail stack
+                let b = List.head stack
+                stack <- List.tail stack
+                stack <- (a * b) :: stack
+            | _ -> ()
 
-        // Input values
-        let test () =
-            let expressions = ["3 + 5"; "3 + 5 * 2"; "(3 + 5) * 2"; "10"; "2 * 3 + 4 * 5"]
-            expressions
-            |> List.iter (fun expr -> printfn "%s = %f" expr (interpret expr))
+        List.head stack
 
-        test ()
+    // Interpret input string
+    let interpret input =
+        let tokens = tokenize input
+        let postfix = infixToPostfix tokens
+        evaluatePostfix postfix
+
+    // Input values
+    let test () =
+        let expressions = [ "3 + 5"; "3 + 5 * 2"; "(3 + 5) * 2"; "10"; "2 * 3 + 4 * 5" ]
+        expressions |> List.iter (fun expr -> printfn "%s = %f" expr (interpret expr))
+
+    test ()
