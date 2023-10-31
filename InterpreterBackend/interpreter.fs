07a60adb0ce14dc30a8badc3fd2546af872cd457
James Sparling <jamesdsparling@gmail.com>
2023-10-31 19:35:33 +0000

    Consistent formatting

---
+++ InterpreterBackend/interpreter.fs
@@ -0,0 +1,116 @@
+﻿// interpreter.fs
+namespace InterpreterBackend
+
+module Interpreter =
+
+    open Lexer
+
+    let parseError = System.Exception("Parser error")
+
+    type AngleMode = Degrees | Radians
+    let toRadians = System.Math.PI / 180.0
+
+    let initialSymbolTable =
+        Map.ofList [
+            "x", 10.0;   // Example variable "x" with an initial value
+            "y", 20.0;   // Another example variable "y" with an initial value
+        ]
+
+    // Define a symbol table (variableName -> variableValue)
+    let mutable symbolTable = Map.empty<string, float>
+
+    // Function to look up variable values
+    let lookupVariable variableName =
+        match Map.tryFind variableName symbolTable with
+        | Some value -> value
+        | None -> raise parseError
+
+    let rec evaluateExpr tList mode =
+        let convertAngle mode value =
+            match mode with
+            | Degrees -> value * toRadians
+            | Radians -> value
+
+        // Expression - addition & subtraction
+        let rec E tList = (T >> Eopt) tList 
+        and Eopt (tList, value) = 
+            match tList with
+            | PLUS :: tail ->
+                let (tLst, tval) = T tail
+                Eopt (tLst, value + tval)
+            | MINUS :: tail ->
+                let (tLst, tval) = T tail
+                Eopt (tLst, value - tval)
+            | _ -> (tList, value)
+
+        // Term - multiplication, division & remainder
+        and T tList = (P >> Topt) tList
+        and Topt (tList, value) =
+            match tList with
+            | TIMES :: tail ->
+                let (tLst, tval) = P tail
+                Topt (tLst, value * tval)
+            | DIVIDE :: tail ->
+                let (tLst, tval) = P tail
+                Topt (tLst, value / tval)
+            | REMAINDER :: tail ->
+                let (tLst, tval) = P tail
+                Topt (tLst, value % tval) 
+            | _ -> (tList, value)
+
+        // Power - exponent operations
+        and P tList = (NR >> Popt) tList
+        and Popt (tList, value) =
+            match tList with
+            | POWER :: tail ->
+                let (tLst, tval) = NR tail
+                Popt (tLst, System.Math.Pow(value, tval))
+            | _ -> (tList, value)
+
+        // Numeric/Parenthesized - numbers, unary operations & functions
+        and NR tList =
+            match tList with 
+            | INTEGER value :: tail -> (tail, value)
+            | FLOAT value :: tail -> (tail, value)
+            | VARIABLE vName :: tail ->
+                let variableValue = lookupVariable vName
+                (tail, variableValue)
+            | MINUS :: tail ->
+                let (tLst, tval) = NR tail
+                (tLst, -tval)
+            | SIN :: tail ->
+                let (tLst, tval) = NR tail
+                (tLst, System.Math.Sin(convertAngle mode tval))
+            | COS :: tail ->
+                let (tLst, tval) = NR tail
+                (tLst, System.Math.Cos(convertAngle mode tval))
+            | TAN :: tail ->
+                let (tLst, tval) = NR tail
+                (tLst, System.Math.Tan(convertAngle mode tval))
+            | LPAREN :: tail ->
+                let (tLst, tval) = E tail
+                match tLst with 
+                | RPAREN :: tail -> (tail, tval)
+                | _ -> raise parseError
+            | _ -> raise parseError
+
+        let VA tList =
+            match tList with
+            | VARIABLE vName :: tail -> 
+                match tail with 
+                | EQUATION :: tail ->
+                    let (tLst, tval) = E tail
+                    // Update the symbol table with the variable assignment
+                    symbolTable <- Map.add vName tval symbolTable
+                    (tLst, tval)
+                | _ -> (E tList)
+            | _ -> (E tList)
+        VA tList
+
+
+    let interpret input mode =
+        let tokens = lexer input
+        let _, result = evaluateExpr tokens mode
+        result
+
+
---
+++ InterpreterGUI/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InterpreterBackend;
+
+namespace InterpreterGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunInterpreter()
+        {
+                if (!(string.IsNullOrWhiteSpace(txtInput.Text)))
+                {
+                    try
+                    {
+                        var result = Interpreter.interpret(txtInput.Text, mode: Interpreter.AngleMode.Radians);
+                        labOutput.Content = result;
+                        labOutput.Foreground = new SolidColorBrush(Colors.White);
+                    } 
+                    catch (Exception ex)
+                    {
+                        labOutput.Content = "Error: " + ex.Message;
+                        labOutput.Foreground = new SolidColorBrush(Colors.Red);
+                    }
+                }
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                RunInterpreter();
+            } else
+            {
+                labOutput.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+    }
+}
