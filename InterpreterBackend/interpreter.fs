7d83d0f77da3ad39e7a0c69165e71b4be5ccdd65
WilliamG99 <19willgarcia99@gmail.com>
2023-11-19 18:56:16 +0000

    Small changes

--- InterpreterBackend/interpreter.fs
+++ InterpreterBackend/interpreter.fs
@@ -22,7 +22,7 @@ module Interpreter =
         | Some value -> value
         | None -> raise parseError
 
-    let rec evaluateExpr tList mode symbolTable =
+    let rec evaluateExpr tList mode (symbolTable: Map<string, float>) =
         let convertAngle mode value =
             match mode with
             | Degrees -> value * toRadians
@@ -30,78 +30,84 @@ module Interpreter =
 
         // Expression - addition & subtraction
         let rec E tList = (T >> Eopt) tList 
-        and Eopt (tList, value) = 
+        and Eopt (tList, (vID, value)) = 
             match tList with
             | PLUS :: tail ->
-                let (tLst, tval) = T tail
-                Eopt (tLst, value + tval)
+                let (tLst, (vID, tval)) = T tail
+                Eopt (tLst, (vID, value + tval))
             | MINUS :: tail ->
-                let (tLst, tval) = T tail
-                Eopt (tLst, value - tval)
-            | _ -> (tList, value)
+                let (tLst, (vID, tval)) = T tail
+                Eopt (tLst, (vID, value - tval))
+            | _ -> (tList, ("", value))
 
         // Term - multiplication, division & remainder
         and T tList = (P >> Topt) tList
-        and Topt (tList, value) =
+        and Topt (tList, (vID, value)) =
             match tList with
             | TIMES :: tail ->
-                let (tLst, tval) = P tail
-                Topt (tLst, value * tval)
+                let (tLst, (vID, tval)) = P tail
+                Topt (tLst, (vID, value * tval))
             | DIVIDE :: tail ->
-                let (tLst, tval) = P tail
-                Topt (tLst, value / tval)
+                let (tLst, (vID, tval)) = P tail
+                Topt (tLst, (vID, value / tval))
             | REMAINDER :: tail ->
-                let (tLst, tval) = P tail
-                Topt (tLst, value % tval) 
-            | _ -> (tList, value)
+                let (tLst, (vID, tval)) = P tail
+                Topt (tLst, (vID, value % tval))
+            | _ -> (tList, ("", value))
 
         // Power - exponent operations
         and P tList = (NR >> Popt) tList
-        and Popt (tList, value) =
+        and Popt (tList, (vID, value)) =
             match tList with
             | POWER :: tail ->
-                let (tLst, tval) = NR tail
-                Popt (tLst, System.Math.Pow(value, tval))
-            | _ -> (tList, value)
+                let (tLst, (vID, tval)) = NR tail
+                Popt (tLst, (vID, System.Math.Pow(value, tval)))
+            | _ -> (tList, ("", value))
 
         // Numeric/Parenthesized - numbers, unary operations & functions
         and NR tList =
             match tList with 
-            | INTEGER value :: tail -> (tail, value)
-            | FLOAT value :: tail -> (tail, value)
+            | INTEGER value :: tail -> (tail, ("", value))
+            | FLOAT value :: tail -> (tail, ("", value))
             | MINUS :: tail ->
-                let (tLst, tval) = NR tail
-                (tLst, -tval)
+                let (tLst, (vID, tval)) = NR tail
+                (tLst, ("", -tval))
             | SIN :: tail ->
-                let (tLst, tval) = NR tail
-                (tLst, System.Math.Sin(convertAngle mode tval))
+                let (tLst, (vID, tval)) = NR tail
+                (tLst, ("", System.Math.Sin(convertAngle mode tval)))
             | COS :: tail ->
-                let (tLst, tval) = NR tail
-                (tLst, System.Math.Cos(convertAngle mode tval))
+                let (tLst, (vID, tval)) = NR tail
+                (tLst, ("", System.Math.Cos(convertAngle mode tval)))
             | TAN :: tail ->
-                let (tLst, tval) = NR tail
-                (tLst, System.Math.Tan(convertAngle mode tval))
+                let (tLst, (vID, tval)) = NR tail
+                (tLst, ("", System.Math.Tan(convertAngle mode tval)))
             | LPAREN :: tail ->
-                let (tLst, tval) = E tail
+                let (tLst, (vID, tval)) = E tail
                 match tLst with 
-                | RPAREN :: tail -> (tail, tval)
+                | RPAREN :: tail -> (tail, ("", tval))
                 | _ -> raise parseError
             // Variable
+            | VARIABLE vName :: tail -> 
+                let res = lookupVariable vName initialSymbolTable
+                (tail, (vName, res))
+            | _ -> raise parseError
+
+        // Variable assignment
+        let VA tList =
+            match tList with
             | VARIABLE vName :: tail ->
                 match tail with
                 | EQUATION :: tail ->
-                    let (tLst, tval) = E tail
-                    let newSymbolTable = Map.add vName tval initialSymbolTable
-                    (tLst, tval)
-
-                | _ -> (tail, lookupVariable vName initialSymbolTable)
-            | _ -> raise parseError
-
-
-        E tList
+                    let (tLst, (vID, tval)) = E tail
+                    (tLst, (vName, tval))            
+                | _ -> E tList
+            | _ -> E tList
+        VA tList
 
 
     let interpret input mode =
         let tokens = lexer input
-        let _, result = evaluateExpr tokens mode initialSymbolTable
-        result
+        let _, result = evaluateExpr tokens mode initialSymbolTable    
+        let vName = fst result
+        let vValue = snd result
+        vValue
